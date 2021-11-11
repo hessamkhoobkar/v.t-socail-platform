92314ef85254df4edd2473452d2eee3a29878821
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2021-11-12 00:03:35 +0330

    new update

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var quill = new Quill("#editor", {
+  modules: {
+    toolbar: [
+      ["bold", "italic", "underline", "strike"],
+      [{ header: 1 }, { header: 2 }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ color: [] }, { background: [] }],
+    ],
+  },
+  placeholder:
+    "متن مختصری برای معرفی خود بنویسید، این متن در صفحه پروفایل شما نمایش داده خواهد شد.",
+  theme: "snow",
+});
+
+const profileCoverInput = document.getElementById("profileCover");
+const profileCoverPreview = document.getElementById("profileCoverPreview");
+
+profileCoverInput.addEventListener("change", (event) => {
+  const [file] = profileCoverInput.files;
+  if (file) {
+    profileCoverPreview.src = URL.createObjectURL(file);
+    profileCoverPreview.classList.remove("fix-hidden");
+  }
+});
